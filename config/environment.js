7fb8bb29cc90acef2014b6b96abdc3504f5720d1
Justin Steenhuis <justinsteenhuis@gmail.com>
2016-03-25 23:08:46 +0100

    Firebase back-end

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'just-in-time',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://just-in-time.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
